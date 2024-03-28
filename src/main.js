03b719a3c20e986982b683a712db7585d2a48b5e
louis <1661800976@qq.com>
2024-03-28 21:48:21 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './assets/main.css'
+
+createApp(App).mount('#app')
+import { createApp } from 'vue'
+
+app.mount('#app')
+app.mount('#trial1')
+app.mount('msg')
+app.mount('state')
+
